60d4bd4e2a5ab2f0f7a8d9819a83ce17970a0c66
Naim Ahmad <naimahmad201103@gmail.com>
2023-11-05 15:49:39 +0600

    sign in page design completed

--- src/layout/StickyNavbar.jsx
+++ src/layout/StickyNavbar.jsx
@@ -3,7 +3,7 @@ import {
   Collapse,
   IconButton,
   Navbar,
-  Typography
+  Typography,
 } from "@material-tailwind/react";
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
@@ -26,7 +26,14 @@ export default function StickyNavbar() {
         color="blue-gray"
         className="p-1 font-normal"
       >
-        <NavLink to="/" className={({isActive})=> isActive ? "flex items-center text-violet-500 font-extrabold": "flex items-center hover:text-violet-500"}>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? "flex items-center text-violet-500 font-extrabold"
+              : "flex items-center hover:text-violet-500"
+          }
+        >
           Home
         </NavLink>
       </Typography>
@@ -36,7 +43,14 @@ export default function StickyNavbar() {
         color="blue-gray"
         className="p-1 font-normal"
       >
-        <NavLink to="/all-jobs" className={({isActive})=> isActive ? "flex items-center text-violet-500 font-extrabold": "flex items-center hover:text-violet-500"}>
+        <NavLink
+          to="/all-jobs"
+          className={({ isActive }) =>
+            isActive
+              ? "flex items-center text-violet-500 font-extrabold"
+              : "flex items-center hover:text-violet-500"
+          }
+        >
           All Jobs
         </NavLink>
       </Typography>
@@ -46,7 +60,14 @@ export default function StickyNavbar() {
         color="blue-gray"
         className="p-1 font-normal"
       >
-        <NavLink to="/post-jobs" className={({isActive})=> isActive ? "flex items-center text-violet-500 font-extrabold": "flex items-center hover:text-violet-500"}>
+        <NavLink
+          to="/post-jobs"
+          className={({ isActive }) =>
+            isActive
+              ? "flex items-center text-violet-500 font-extrabold"
+              : "flex items-center hover:text-violet-500"
+          }
+        >
           Post A Job
         </NavLink>
       </Typography>
@@ -56,7 +77,14 @@ export default function StickyNavbar() {
         color="blue-gray"
         className="p-1 font-normal"
       >
-        <NavLink to="/blogs" className={({isActive})=> isActive ? "flex items-center text-violet-500 font-extrabold": "flex items-center hover:text-violet-500"}>
+        <NavLink
+          to="/blogs"
+          className={({ isActive }) =>
+            isActive
+              ? "flex items-center text-violet-500 font-extrabold"
+              : "flex items-center hover:text-violet-500"
+          }
+        >
           Blogs
         </NavLink>
       </Typography>
@@ -64,75 +92,79 @@ export default function StickyNavbar() {
   );
 
   return (
-    <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
-      <div className="flex items-center justify-between text-blue-gray-900">
-        <Typography
-          as="a"
-          href="#"
-          className="mr-4 cursor-pointer py-1.5 font-extrabold text-slate-900 md:text-2xl"
-        >
-          Click <span className="text-violet-500">Jobs</span>
-        </Typography>
-        <div className="flex items-center gap-4">
-          <div className="mr-4 hidden lg:block">{navList}</div>
-          <div className="flex items-center gap-x-1">
-            
-            <Button
-              variant="gradient"
-              size="sm"
-              color="violet-500"
-              className="hidden lg:inline-block bg-violet-500"
+    <Navbar className="sticky top-0 max-w-full z-10 h-max rounded-none px-4 py-2 lg:px-8 lg:py-4">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex items-center justify-between text-blue-gray-900">
+          <Typography
+            as="a"
+            href="#"
+            className="mr-4 cursor-pointer py-1.5 font-extrabold text-slate-900 md:text-2xl"
+          >
+            Click <span className="text-violet-500">Jobs</span>
+          </Typography>
+          <div className="flex items-center gap-4">
+            <div className="mr-4 hidden lg:block">{navList}</div>
+            <div className="flex items-center gap-x-1">
+              <Button
+                variant="gradient"
+                size="sm"
+                color="violet-500"
+                className="hidden lg:inline-block bg-violet-500"
+              >
+                <Link to="/sign-in">
+                  {" "}
+                  <span>Sign in</span>
+                </Link>
+              </Button>
+            </div>
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
             >
-             <Link to="/sign-in"> <span>Sign in</span></Link>
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
+          </div>
+        </div>
+        <Collapse open={openNav}>
+          {navList}
+          <div className="flex items-center gap-x-1">
+            <Button fullWidth variant="gradient" size="sm" className="">
+              <span>Sign in</span>
             </Button>
           </div>
-          <IconButton
-            variant="text"
-            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-            ripple={false}
-            onClick={() => setOpenNav(!openNav)}
-          >
-            {openNav ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                className="h-6 w-6"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            )}
-          </IconButton>
-        </div>
+        </Collapse>
       </div>
-      <Collapse open={openNav}>
-        {navList}
-        <div className="flex items-center gap-x-1">
-          <Button fullWidth variant="gradient" size="sm" className="">
-            <span>Sign in</span>
-          </Button>
-        </div>
-      </Collapse>
     </Navbar>
   );
 }
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,7 +1,49 @@
+import {
+    Button,
+    Card,
+    CardBody,
+    CardFooter,
+    CardHeader,
+    Input,
+    Typography
+} from "@material-tailwind/react";
 
 export default function SignIn() {
-
-    return (
-        <div>SignIn</div>
-    )
+  return (
+    <div className="flex items-center justify-center min-h-[80svh]">
+      <Card className="w-96">
+        <CardHeader
+          variant="gradient"
+          color="gray"
+          className="mb-4 grid h-28 place-items-center"
+        >
+          <Typography variant="h3" color="white">
+            Sign In
+          </Typography>
+        </CardHeader>
+        <CardBody className="flex flex-col gap-4">
+          <Input label="Email" size="lg" />
+          <Input label="Password" size="lg" />
+          
+        </CardBody>
+        <CardFooter className="pt-0">
+          <Button variant="gradient" fullWidth>
+            Sign In
+          </Button>
+          <Typography variant="small" className="mt-6 flex justify-center">
+            Don&apos;t have an account?
+            <Typography
+              as="a"
+              href="#signup"
+              variant="small"
+              color="blue-gray"
+              className="ml-1 font-bold"
+            >
+              Sign up
+            </Typography>
+          </Typography>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
