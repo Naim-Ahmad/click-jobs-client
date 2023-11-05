a8d8754c9dd707545360bd1eb23cc089c1d7f1f3
Naim Ahmad <naimahmad201103@gmail.com>
2023-11-05 13:07:51 +0600

    navbar created

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "./Navbar";
+// import ComplexNavbar from "./ComplexNavbar";
+import StickyNavbar from "./StickyNavbar";
 
 export default function MainLayout() {
 
     return (
         <div>
-            <Navbar/>
+            <StickyNavbar/>
+            {/* <ComplexNavbar/> */}
             <Outlet></Outlet>
         </div>
     )
