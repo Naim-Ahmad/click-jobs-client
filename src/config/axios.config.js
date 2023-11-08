1125d301609c138521eb74cbd14705e129074260
Naim Ahmad <naimahmad201103@gmail.com>
2023-11-08 23:52:08 +0600

    finished assignment

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -1,8 +1,8 @@
 import axiosLibrary from "axios";
 
 const axios = axiosLibrary.create({
-    baseURL: "http://localhost:5000",
-    // baseURL: "https://click-jobs-server.vercel.app"
+    // baseURL: "http://localhost:5000",
+    baseURL: "https://click-jobs-server.vercel.app"
 });
 
 export default axios
