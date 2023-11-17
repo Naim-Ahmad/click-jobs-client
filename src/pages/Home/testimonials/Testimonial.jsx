91832c7c6a0c30fbfa86e8f3994c9cce2dc32972
Naim Ahmad <naimahmad201103@gmail.com>
2023-11-17 18:08:49 +0600

    fixed some bug

--- src/pages/Home/testimonials/Testimonial.jsx
+++ src/pages/Home/testimonials/Testimonial.jsx
@@ -32,7 +32,7 @@ export default function Testimonial() {
           </SectionDescription>
         </SectionHeader>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pb-16">
           
          {data?.map(d=> <TestimonialCard key={d._id} data={d}/>)}
         </div>
